372982ff028d3b4d7171731a27d2405022cb7429
sylar <sylar@Sylar-Mac-Mini-2.local>
2016-10-19 16:54:47 +0800

    init

---
+++ SimpleFunction/SimpleFunction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleFunction
+//
+//  Created by sylar on 2016/10/19.
+//  Copyright © 2016年 sylar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleFunction/SimpleFunction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleFunction
+//
+//  Created by sylar on 2016/10/19.
+//  Copyright © 2016年 sylar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
